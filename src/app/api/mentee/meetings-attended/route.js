9abafbebe06c2bceea6311dfbf6986f3e7c9219e
Shivank Goel <shivankgoel600@gmail.com>
2025-01-23 10:39:38 +0530

    Fix mentor dashboard

--- src/app/api/mentee/meetings-attended/route.js
+++ src/app/api/mentee/meetings-attended/route.js
@@ -2,6 +2,45 @@ import { connect } from "../../../../lib/dbConfig";
 import { Mentee } from "../../../../lib/dbModels";
 import { NextResponse } from "next/server";
 
+export async function POST(req) {
+  try {
+    const { meeting_id, presentMentees, totalMentees } = await req.json();
+    await connect();
+
+    // console.log("Attendance updated", meeting_id, presentMentees, totalMentees);
+
+    // Add meeting to present mentees
+    for (const menteeId of presentMentees) {
+      await Mentee.findOneAndUpdate(
+        { MUJid: menteeId, meetingsAttended: { $ne: meeting_id } },
+        {
+          $push: { meetingsAttended: meeting_id },
+        }
+      );
+    }
+
+    // Remove meeting from absent mentees
+    const absentMentees = totalMentees.filter(
+      (id) => !presentMentees.includes(id)
+    );
+    for (const menteeId of absentMentees) {
+      await Mentee.findOneAndUpdate(
+        { MUJid: menteeId },
+        {
+          $pull: { meetingsAttended: meeting_id },
+        }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Attendance updated successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
 export async function PUT(req) {
   try {
     const { MUJid, mentorRemarks } = await req.json();
